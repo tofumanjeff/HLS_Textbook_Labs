0a9a34ead64f8e37053316e233f48699fd08c6e8
Chia-Fu Liu <tofumanjeff@gmail.com>
2021-08-02 21:31:55 +0800

    Update solution.tcl

--- pipe/loop_pipe/reference_report/solution.tcl
+++ pipe/loop_pipe/reference_report/solution.tcl
@@ -1,3 +1,8 @@
+###############################################
+# Copyright (C) Copyright © 2021-2022, BOLEDU #
+# SPDX-License-Identifier: BSD-3-Clause       #
+###############################################
+
 # solution1
 set_directive_pipeline "loop_pipeline/LOOP_J"
 
