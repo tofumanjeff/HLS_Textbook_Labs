75461f503e66c761ad1a67abbc0e0b6bbc9708f0
Chia-Fu Liu <tofumanjeff@gmail.com>
2021-08-02 21:21:53 +0800

    New directory structure / Tcl scripts

---
+++ pipe/loop_pipe/reference_report/solution.tcl
@@ -0,0 +1,8 @@
+# solution1
+set_directive_pipeline "loop_pipeline/LOOP_J"
+
+# solution2
+set_directive_pipeline "loop_pipeline/LOOP_I"
+
+# solution3
+set_directive_pipeline "loop_pipeline"
---
+++ pipe/loop_pipe/run_hls.tcl
@@ -0,0 +1,25 @@
+###############################################
+# Copyright (C) Copyright © 2021-2022, BOLEDU #
+# SPDX-License-Identifier: BSD-3-Clause       #
+###############################################
+
+# Create a project
+open_project -reset loop_pipe_prj
+
+# Set the top-level function
+set_top loop_pipeline
+
+# Add source files
+add_files loop_pipeline.cpp
+add_files loop_pipeline.h
+add_files -tb loop_pipeline_test.cpp
+add_files -tb result.golden.dat
+
+# Create a solution
+open_solution -reset "solution1" -flow_target vivado
+
+# Define technology and clock rate
+set_part {xc7z020clg400-1}
+create_clock -period 10 -name default
+
+exit
