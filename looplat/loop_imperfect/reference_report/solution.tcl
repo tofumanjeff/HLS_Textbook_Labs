3b71dcb2f49cc5372ab43cb3f4e81071102f84ca
Chia-Fu Liu <tofumanjeff@gmail.com>
2021-08-06 22:33:16 +0800

    loop_imperfect first commit

---
+++ looplat/loop_imperfect/reference_report/solution.tcl
@@ -0,0 +1,10 @@
+###############################################
+# Copyright (C) Copyright © 2021-2022, BOLEDU #
+# SPDX-License-Identifier: BSD-3-Clause       #
+###############################################
+
+# loop_imperfect
+set_directive_pipeline "loop_imperfect/LOOP_J"
+
+# loop_perfect
+set_directive_pipeline "loop_perfect/LOOP_J"
