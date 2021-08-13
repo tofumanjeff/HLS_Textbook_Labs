4c080bbf32baa97164d51a6c2cd2d560d0915114
Chia-Fu Liu <tofumanjeff@gmail.com>
2021-08-13 21:53:56 +0800

    Update loop_merge.h

--- pipe/loop_merge/loop_merge.h
+++ pipe/loop_merge/loop_merge.h
@@ -3,9 +3,14 @@
 * SPDX-License-Identifier: BSD-3-Clause       *
 **********************************************/
 
+#ifndef _LOOP_MERGE_H_
+#define _LOOP_MERGE_H_
+
 #define N 5
 
 typedef int data_t;
 
 // Top function
 void loop_merge(data_t a[N], data_t b[N], data_t c[N], bool d[N]);
+
+#endif
