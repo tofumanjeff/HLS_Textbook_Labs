c7854e368bac4f13109fdaae837eeabc75cf3386
Chia-Fu Liu <tofumanjeff@gmail.com>
2021-08-14 19:24:51 +0800

    assertion first commit

---
+++ pipe/assertion/reference_report/solution.tcl
@@ -0,0 +1,10 @@
+###############################################
+# Copyright (C) Copyright © 2021-2022, BOLEDU #
+# SPDX-License-Identifier: BSD-3-Clause       #
+###############################################
+
+# solution1
+## No assertions
+
+# solution2
+## Add assertions
